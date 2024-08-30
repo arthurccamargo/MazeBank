41e7f132f9fdad3fc05c5d41f81855e459bf3598
Arthur <arthurcalazans@gmail.com>
2024-08-30 15:27:30 -0300

    Check Limit

--- src/main/java/com/arthurccamargo/mazebank/rest/controller/TransactionController.java
+++ src/main/java/com/arthurccamargo/mazebank/rest/controller/TransactionController.java
@@ -1,7 +1,11 @@
 package com.arthurccamargo.mazebank.rest.controller;
 
+import com.arthurccamargo.mazebank.domain.entities.CheckingAccount;
 import com.arthurccamargo.mazebank.domain.entities.Transaction;
+import com.arthurccamargo.mazebank.exception.OutOfLimitException;
+import com.arthurccamargo.mazebank.rest.dto.InfoAccountDTO;
 import com.arthurccamargo.mazebank.rest.dto.TransactionDTO;
+import com.arthurccamargo.mazebank.service.CheckingAccountService;
 import com.arthurccamargo.mazebank.service.TransactionService;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,15 +16,30 @@ import static org.springframework.http.HttpStatus.CREATED;
 public class TransactionController {
 
     private final TransactionService service;
+    private final CheckingAccountService checkingAccountService;
 
-    public TransactionController(TransactionService service) {
+    public TransactionController(TransactionService service, CheckingAccountService checkingAccountService) {
         this.service = service;
+        this.checkingAccountService = checkingAccountService;
     }
 
     @PostMapping
     @ResponseStatus(CREATED)
     public Long save(@RequestBody TransactionDTO dto) {
-        Transaction transaction = service.save(dto);
-        return transaction.getId();
+        Long checkingSenderAccount =  dto.getSenderAccount();
+        InfoAccountDTO infoAccountDTO = checkingAccountService.findById(checkingSenderAccount);
+
+        if(checkLimit(dto.getAmount(), infoAccountDTO.getTransaction_limit())) {
+            Transaction transaction = service.save(dto);
+            return transaction.getId();
+        }
+        else {
+            throw new OutOfLimitException();
+        }
+    }
+
+    private boolean checkLimit(Double amount, Integer limit) {
+        return amount <= limit;
     }
 }
+
